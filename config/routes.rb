2d0d35b79c275cd85d4416951e921213afad4896
Satoshi Odashima <odashimasatoshi@odajimasatoshinoMacBook-puro.local>
2019-03-31 11:02:52 +0900

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get 'view/index'
+  root "users#login_form"
+  get "view/index"
+  get "login" => "users#login_form"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
