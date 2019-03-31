70e38bafc608e408815322e620da1ef93c9700b5
Satoshi Odashima <odashimasatoshi@odajimasatoshinoMacBook-puro.local>
2019-03-31 15:15:45 +0900

    ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :name, {presence: true}
+  validates :sex, {presence: true}
+  validates :birthday, {presence: true}
+  validates :password, {presence: true}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_31_055306) do
+ActiveRecord::Schema.define(version: 2019_03_31_061404) do
 
   create_table "employees", force: :cascade do |t|
     t.string "content"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2019_03_31_055306) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password"
+    t.string "email"
   end
 
 end
