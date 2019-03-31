cfc3aca0646f60e06b6565a6b0b8ba00fa6f42ab
Satoshi Odashima <odashimasatoshi@odajimasatoshinoMacBook-puro.local>
2019-04-01 08:53:04 +0900

    ok

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,20 @@
 class UsersController < ApplicationController
+
   def login_form
+
+  end
+
+  def login
+    @user = User.find_by(email: params[:email], password: params[:password])
+    if @users
+      flash[:notice] = "ログインしました"
+      redirect_to("/view/index")
+    else
+      @error_message = "メールアドレスまたはパスワードが間違っています"
+      @email = params[:email]
+      @password = params[:password]
+
+      render("users/login_form")
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+
   root "users#login_form"
   get "view/index"
   get "login" => "users#login_form"
+  post "login" => "users#login"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
