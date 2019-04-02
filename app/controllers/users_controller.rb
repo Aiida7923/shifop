4f9cbdb1e02ae5adc1735eabb5107b0182c20478
Satoshi Odashima <odashimasatoshi@odajimasatoshinoMacBook-puro.local>
2019-04-02 17:41:46 +0900

    ok

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,26 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user
 
   def login_form
-
   end
 
   def login
     @user = User.find_by(email: params[:email], password: params[:password])
     if @user
+      session[:user_id] = @user.id
       flash[:notice] = "ログインしました"
       redirect_to("/view/index")
     else
       @error_message = "メールアドレスまたはパスワードが間違っています"
       @email = params[:email]
       @password = params[:password]
-
       render("users/login_form")
     end
+  end
+
+  def logout
+    session[:user_id] = nil
+    flash[:notice] = "ログアウトしました"
+    redirect_to("/login")
   end
 end
