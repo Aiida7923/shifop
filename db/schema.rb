9f5021bd1c5d41b889585c8fb486a7bd90890988
Satoshi Odashima <odashimasatoshi@odajimasatoshinoMacBook-puro.local>
2019-05-29 02:14:01 +0900

    edited Gemfile and database.yml

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_17_101315) do
+ActiveRecord::Schema.define(version: 2019_04_30_011949) do
+
+  create_table "events", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.integer "user_id"
