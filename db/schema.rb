3d2f19ca9b0233aef108e3b2bc12ba7f4d503485
Satoshi Odashima <odashimasatoshi@odajimasatoshinoMacBook-puro.local>
2019-06-01 03:50:32 +0900

    introduce password encryption

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_30_011949) do
+ActiveRecord::Schema.define(version: 2019_05_31_183259) do
 
   create_table "events", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -30,11 +30,10 @@ ActiveRecord::Schema.define(version: 2019_04_30_011949) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password"
     t.string "email"
+    t.string "password_digest"
   end
 
 end
